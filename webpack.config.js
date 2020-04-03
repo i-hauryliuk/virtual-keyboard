3483b2cf7a0bae19104931c475601a77a7a95724
Ihar Hauryliuk <ihar.hauryliuk@gmail.com>
2020-04-03 23:15:32 +0300

    refactor: install and setup development environment
      - webpack
      - babel
      - eslint
      - sass
      - postcss

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+
+  const config = {
+    devtool: isProduction ? 'none' : 'source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      host: '0.0.0.0',
+      port: 8000,
+    },
+    entry: [
+      './src/main.js',
+      './src/scss/main.scss',
+    ],
+    output: {
+      filename: 'js/script.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: !isProduction,
+                importLoaders: 2,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: !isProduction,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: !isProduction,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/style.css',
+      }),
+    ],
+  };
+
+  if (isProduction) {
+    config.plugins.unshift(new CleanWebpackPlugin());
+  }
+
+  return config;
+};
